a2a5a38e61556c14e2bf8220a96fcceb858f91a1
Boris Kayi <boriskayienzo@gmail.com>
2021-02-24 11:36:07 +0200

    chore:minor changes

---
+++ lib/views/widgets/reusables/image_field.dart
@@ -0,0 +1,102 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:todo/utils/constants.dart';
+import 'package:todo/utils/styles.dart';
+
+class ImageField extends StatelessWidget {
+  const ImageField({
+    Key key,
+    @required ImagePicker picker,
+    @required ValueNotifier<File> imageNotifier,
+  })  : _picker = picker,
+        _imageNotifier = imageNotifier,
+        super(key: key);
+
+  final ImagePicker _picker;
+  final ValueNotifier<File> _imageNotifier;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => showModalBottomSheet(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+        )),
+        backgroundColor: TodoColors.accent,
+        barrierColor: Colors.blueGrey.withOpacity(1 / 5),
+        context: context,
+        builder: (context) => Container(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 30),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: TodoColors.lightGrey),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.camera_alt,
+                    color: TodoColors.deepDark,
+                    size: 30,
+                  ),
+                  onPressed: () async {
+                    final image = await _picker.getImage(
+                      source: ImageSource.camera,
+                    );
+                    if (image != null) _imageNotifier.value = File(image.path);
+                  },
+                ),
+              ),
+              SizedBox(width: 20),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: TodoColors.lightGrey),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.image,
+                    color: TodoColors.deepDark,
+                    size: 30,
+                  ),
+                  onPressed: () async {
+                    final image = await _picker.getImage(
+                      source: ImageSource.gallery,
+                    );
+                    if (image != null) _imageNotifier.value = File(image.path);
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      child: _imageNotifier.value == null
+          ? Container(
+              width: double.infinity,
+              height: 130,
+              decoration: BoxDecoration(
+                color: TodoColors.lightGrey,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Center(
+                child: Text(
+                  "Tap to add an image",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            )
+          : Container(
+              height: screenWidth(context) / 1.5,
+              constraints: BoxConstraints(maxHeight: 500),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: FileImage(_imageNotifier.value), fit: BoxFit.cover),
+              ),
+            ),
+    );
+  }
+}
